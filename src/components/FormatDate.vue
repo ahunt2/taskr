1a1476d707845edd3d19489c5d4056091cef0ec4
Andrew Hunt <andrew.hunt.5309@gmail.com>
2022-07-06 19:44:40 -0500

    Added format date component

---
+++ src/components/FormatDate.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const props = defineProps(['date'])
+const formatedDate = computed(() => {
+  const day = props.date.getDate()
+  const month = props.date.getMonth()
+  const year = props.date.getFullYear()
+
+  return `${day}/${month}/${year}`
+})
+
+</script>
+
+<template>
+  <h1>{{ formatedDate }}</h1>
+</template>
